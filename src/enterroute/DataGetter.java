647a353d5c031f2e1695fd459e03a6dfcc3d788d
DanielVanDijkJacobsen <danilar@ruc.dk>
2019-11-13 20:42:13 +0100

    Removed unused import
    Not using arraylists somehow.

--- src/enterroute/DataGetter.java
+++ src/enterroute/DataGetter.java
@@ -9,7 +9,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.sql.*;
-import java.util.ArrayList;
 
 public class DataGetter {
     Connection connection;
